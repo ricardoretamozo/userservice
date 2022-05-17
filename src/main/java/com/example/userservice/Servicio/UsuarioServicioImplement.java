c9dc71570f8da92aa478f80f8f44856caee84f96
ricardoretamozo <ricardo.retamozo.garay@gmail.com>
2022-05-16 19:03:16 -0500

    Auth completo.

--- src/main/java/com/example/userservice/Servicio/UsuarioServicioImplement.java
+++ src/main/java/com/example/userservice/Servicio/UsuarioServicioImplement.java
@@ -6,23 +6,32 @@ import com.example.userservice.repo.RolRepo;
 import com.example.userservice.repo.UsuarioRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UsuarioServicioImplement implements UsuarioServicio{
+public class UsuarioServicioImplement implements UsuarioServicio, UserDetailsService {
 
     private final UsuarioRepo usuarioRepo;
     private final RolRepo rolRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Usuario saveUsuario(Usuario usuario) {
         log.info("Se creo un nuevo usuario");
+        usuario.setContrasena(passwordEncoder.encode(usuario.getContrasena()));
         return usuarioRepo.save(usuario);
     }
 
@@ -46,5 +55,20 @@ public class UsuarioServicioImplement implements UsuarioServicio{
     @Override
     public List<Usuario> getUsers() {
         return usuarioRepo.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepo.findByUsuario(username);
+        if (usuario == null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }else {
+            log.info("User found in the database: {}", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        usuario.getRoles().forEach(rol -> authorities.add(new SimpleGrantedAuthority(rol.getNombre())));
+        return new org.springframework.security.core.userdetails.User(usuario.getUsuario(), usuario.getContrasena(),authorities);
     }
 }
--- src/main/java/com/example/userservice/controller/UsuarioController.java
+++ src/main/java/com/example/userservice/controller/UsuarioController.java
@@ -1,16 +1,35 @@
 package com.example.userservice.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.userservice.Servicio.UsuarioServicio;
 import com.example.userservice.modelos.Rol;
 import com.example.userservice.modelos.Usuario;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api")
@@ -37,6 +56,43 @@ public class UsuarioController {
     public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form) {
         usuarioServicio.addRolToUsuario(form.getUsername(), form.getRoleName());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/refreshtoken")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                Usuario usuario = usuarioServicio.getUsuario(username);
+                String access_token=JWT.create()
+                        .withSubject(usuario.getUsuario())
+                        .withExpiresAt(new Date(System.currentTimeMillis()+10*60*1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles",usuario.getRoles().stream().map(Rol::getNombre).collect(Collectors.toList()))
+                        .sign(algorithm);
+                Map<String, String> tokens=new HashMap<>();
+                tokens.put("access_token", access_token);
+                tokens.put("refresh_token", refresh_token);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+            } catch (Exception exception) {
+                response.setHeader("error", exception.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                //response.sendError(FORBIDDEN.value());
+                Map<String, String> error=new HashMap<>();
+                error.put("error_message", exception.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }else {
+            throw new RuntimeException("Refresh token is missing");
+        }
     }
 }
 
